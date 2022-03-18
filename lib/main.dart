94b5a47f8b44ec47fd917017b3fdb0741e6adb72
Sylvia Kang <sylvia@ksvs.kh.edu.tw>
2022-03-18 13:20:17 +0800

    20220318

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'price_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
           primaryColor: Colors.lightBlue,
           scaffoldBackgroundColor: Colors.white),
